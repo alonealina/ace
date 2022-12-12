2838754636568db6492949b6322c974218f07773
bluesky2008 <william_billl2008@yahoo.co.jp>
2022-12-12 18:39:07 +0900

    一旦コミット

---
+++ resources/views/index.blade.php
@@ -0,0 +1,28 @@
+@extends('layouts.app_fw')
+
+@section('content')
+<link rel="stylesheet" type="text/css" href="https://coco-factory.jp/ugokuweb/wp-content/themes/ugokuweb/data/reset.css">
+<div class="wrapper">
+    <ul class="slider">
+        <li class="slider-item slider-item01"></li>
+        <li class="slider-item slider-item02"></li>
+        <li class="slider-item slider-item03"></li>
+    </ul>
+</div>
+
+@endsection
+
+
+@section('content_sp')
+
+<div class="wrapper">
+    <ul class="slider">
+        <li class="slider-item slider-item01_sp"></li>
+        <li class="slider-item slider-item02_sp"></li>
+        <li class="slider-item slider-item03_sp"></li>
+    </ul>
+</div>
+
+
+
+@endsection
