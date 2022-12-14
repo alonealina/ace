fb149e2601b4c984e0e9a75de00db4c21e964c9d
bluesky2008 <william_billl2008@yahoo.co.jp>
2022-12-14 15:35:56 +0900

    メール実装

--- resources/views/mail_comp.blade.php
+++ resources/views/mail_comp.blade.php
@@ -1,18 +1,17 @@
-@extends('layouts.app')
+@extends('layouts.app_fw')
+
+@section('page_name')
+Contact
+@endsection
+
 
 @section('content')
-<div class="page_title_div">
-    <div class="page_title">CONTACT</div>
+<div class="content">
+    <div class="mail_comp_div">
+        お問い合わせありがとうございます。<br>
+        返信が届くまでしばらくお待ちください。
+    </div>
 </div>
-
-<div class="mail_comp_div">
-    お問い合わせありがとうございます。<br>
-    返信が届くまでしばらくお待ちください。
-</div>
-<script src="https://code.jquery.com/jquery-3.4.1.min.js" integrity="sha256-CSXorXvZcTkaix6Yvo6HppcZGetbYMGWSFlBw8HfCJo=" crossorigin="anonymous"></script>
-<script src="{{ asset('js/contact.js') }}"></script>
-
-
 @endsection
 
 
@@ -21,8 +20,6 @@
     <div class="page_title_sp">CONTACT</div>
     <div class="mail_comp_div_sp">お問い合わせありがとうございます。<br>返信が届くまでしばらくお待ちください。</div>
 </div>
-<script src="https://code.jquery.com/jquery-3.4.1.min.js" integrity="sha256-CSXorXvZcTkaix6Yvo6HppcZGetbYMGWSFlBw8HfCJo=" crossorigin="anonymous"></script>
-<script src="{{ asset('js/contact.js') }}"></script>
 
 
 
