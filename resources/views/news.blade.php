4283ed31a30b1292ff42d1a8fdb35a4765799b78
bluesky2008 <william_billl2008@yahoo.co.jp>
2022-12-15 17:05:14 +0900

    NEWS実装

--- resources/views/news.blade.php
+++ resources/views/news.blade.php
@@ -35,14 +35,12 @@ NEWS
     <div class="page_title_sp">NEWS</div>
     <div id="news_list">
         @foreach($news_list as $news)
-        <a href="#" onclick="clickNewsSp({{ $news->id }})" class="news_column">2022.08.08　HPリニューアルオープン</a>
+        <a href="#" onclick="clickNewsSp({{ $news->id }})" class="news_column">{{ $news->notice_date }}　{{ $news->title }}</a>
         @endforeach
     </div>
 
     <div id="news_div">
         <div class="news_text_sp">
-            <div class="news_title_sp">2022.08.08<br>HPリニューアルオープン</div>
-            この度、株式会社Ace＆Co.はホームページをリニューアルオープンする運びとなりました。<br>今後ともAndyグループをよろしくお願いいたします。
         </div>
         <a href="#" id="news_list_btn" class="news_list_btn_sp" onclick="clickNewsBtn()">NEWS一覧へ</a>
     </div>
