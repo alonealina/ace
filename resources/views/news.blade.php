e4245d3888a4a82d81b82a4f7ad2b2aefc5b881b
bluesky2008 <william_billl2008@yahoo.co.jp>
2022-12-15 14:20:02 +0900

    SP NEWS実装

--- resources/views/news.blade.php
+++ resources/views/news.blade.php
@@ -36,14 +36,28 @@ NEWS
 
 @section('content_sp')
 
-<div class="wrapper">
-    <ul class="slider">
-        <li class="slider-item slider-item01_sp"></li>
-        <li class="slider-item slider-item02_sp"></li>
-        <li class="slider-item slider-item03_sp"></li>
-    </ul>
+<div class="content_sp">
+    <div class="page_title_sp">NEWS</div>
+    <div id="news_list">
+        <a href="#" onclick="clickNews(1)" class="news_column">2022.08.08　HPリニューアルオープン</a>
+        <a href="#" onclick="clickNews(2)" class="news_column">2022.08.08　HPリニューアルオープン</a>
+        <a href="#" onclick="clickNews(3)" class="news_column">2022.08.08　HPリニューアルオープン</a>
+        <a href="#" onclick="clickNews(4)" class="news_column">2022.08.08　HPリニューアルオープン</a>
+        <a href="#" onclick="clickNews(5)" class="news_column">2022.08.08　HPリニューアルオープン</a>
+        <a href="#" onclick="clickNews(6)" class="news_column">2022.08.08　HPリニューアルオープン</a>
+    </div>
+
+    <div id="news_div">
+        <div class="news_text_sp">
+            <div class="news_title_sp">2022.08.08<br>HPリニューアルオープン</div>
+            この度、株式会社Ace＆Co.はホームページをリニューアルオープンする運びとなりました。<br>今後ともAndyグループをよろしくお願いいたします。
+        </div>
+        <a href="#" id="news_list_btn" class="news_list_btn_sp" onclick="clickNewsBtn()">NEWS一覧へ</a>
+    </div>
+
 </div>
 
+<script src="{{ asset('js/news.js') }}"></script>
 
 
 @endsection
